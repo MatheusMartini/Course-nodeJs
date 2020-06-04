1e285cb8e359dccd201a61d6811a39e063c19ae2
MatheusMartini <matheus.martini@rede.ulbra.br>
2020-06-04 11:02:02 -0300

    data

--- playground/fileJson.js
+++ playground/fileJson.js
@@ -3,11 +3,14 @@ const fs = require('fs');
 const cars = {
   name: 'mustang',
   marca: 'ford',
-  price: 9999
+  price: 9999,
 };
 const carsJSON = JSON.stringify(cars);
 
-fs.writeFileSync('fileJson.json', carsJSON);
+//fs.writeFileSync('fileJson.json', carsJSON);
+//add in fileJson.json the params pass in const cars 
 
-const carsParse = JSON.parse(carsJSON);
-console.log(carsParse);
+const dataBuffer = fs.readFileSync('fileJson.json');
+const dataJSON = dataBuffer.toString();
+const data = JSON.parse(dataJSON);
+console.log(data);
