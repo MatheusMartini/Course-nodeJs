384d145b0598bec59d426dedfb74144d35929f5d
MatheusMartini <matheus.martini@rede.ulbra.br>
2020-07-06 17:55:36 -0300

    organization

--- playground/fileJson.js
+++ playground/fileJson.js
@@ -9,9 +9,9 @@ const carsJSON = JSON.stringify(cars);
 
 //fs.writeFileSync('fileJson.json', carsJSON);
 //add in fileJson.json the params pass in const cars 
-const dataBuffer = fs.readFileSync('fileJson.json');
-const dataJSON = dataBuffer.toString();
-const data = JSON.parse(dataJSON);
+  const dataBuffer = fs.readFileSync('fileJson.json');
+  const dataJSON = dataBuffer.toString();
+  const data = JSON.parse(dataJSON);
 
 data.name = 'Martini',
 data.age = 20
