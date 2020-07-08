66a62bed7e99282e45b38d1c6ac276a713c54db0
MatheusMartini <matheus.martini@rede.ulbra.br>
2020-07-08 09:32:22 -0300

    add list command

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,57 +1,61 @@
-const fs = require('fs')
+const fs = require("fs");
 
-const getNotes = function () {
-    return 'Your notes...'
-}
+const getNotes = () => {
+  return "Your notes...";
+};
 
-const addNote = function (title, body) {
-    const notes = loadNotes()
-    const duplicateNotes = notes.filter(function (note) {
-        return note.title === title
-    })
+const addNote = (title, body) => {
+  const notes = loadNotes();
+  const duplicateNotes = notes.filter((note) => note.title === title);
 
-    if (duplicateNotes.length === 0) {
-        notes.push({
-            title: title,   
-            body: body
-        })
-        saveNotes(notes)
-        console.log('New note added!')
-    } else {
-        console.log('Note title taken!')
-    }
-}
+  if (duplicateNotes.length === 0) {
+    notes.push({
+      title: title,
+      body: body,
+    });
+    saveNotes(notes);
+    console.log("New note added!");
+  } else {
+    console.log("Note title taken!");
+  }
+};
 
-const removeNote = function(title){
-    const readNotes = loadNotes();
-    const notesToKeep = readNotes.filter(function (note){
-        return note.title !== title
-    })
-    if(readNotes.length > notesToKeep.length){
-        console.log("note removed")
-        saveNotes(notesToKeep)
-    }else{
-        console.log("note not found")
-    }
-}
+const removeNote = (title) => {
+  const readNotes = loadNotes();
+  const notesToKeep = readNotes.filter((note) => note.title !== title);
+  if (readNotes.length > notesToKeep.length) {
+    console.log("note removed");
+    saveNotes(notesToKeep);
+  } else {
+    console.log("note not found");
+  }
+};
 
-const saveNotes = function (notes) {
-    const dataJSON = JSON.stringify(notes)
-    fs.writeFileSync('notes.json', dataJSON)
-}
+const saveNotes = (notes) => {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", dataJSON);
+};
 
-const loadNotes = function () {
-    try {
-        const dataBuffer = fs.readFileSync('notes.json')
-        const dataJSON = dataBuffer.toString()
-        return JSON.parse(dataJSON)
-    } catch (e) {
-        return []
-    }
+const loadNotes = () => {
+  try {
+    const dataBuffer = fs.readFileSync("notes.json");
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (e) {
+    return [];
+  }
+};
+
+const listNotes = () =>{
+    const notes = loadNotes();
+    notes.forEach((note) => {
+        console.log(note.title)
+    });
 }
 
 module.exports = {
-    getNotes: getNotes,
-    addNote: addNote,
-    removeNote: removeNote
-}
+  getNotes: getNotes,
+  addNote: addNote,
+  removeNote: removeNote,
+  listNotes: listNotes
+};
