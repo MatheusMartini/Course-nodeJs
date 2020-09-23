826ce1513bc67fced16c90f016a28ea16203c3fc
MatheusMartini <matheus.martini@rede.ulbra.br>
2020-09-23 16:12:33 -0300

    request http api climate

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,12 @@
 const request = require("request");
-const url = "http://api.weatherstack.com/current?access_key=f6f2d33a9253b6091dfd0f65d44d04ec&query=New%20York";
 
-request({ url: url }, (error, response) => {
-  const data =JSON.parse(response.body)
-  console.log(data)
+const url = "http://api.weatherstack.com/current?access_key=f6f2d33a9253b6091dfd0f65d44d04ec&query=New%20York&units=m";
+
+request({ url: url,json:true }, (error, response) => {
+  // console.log(response.body.current)
+  let temperature = response.body.current.temperature;
+  let probabilityRain = response.body.current.precip;
+  let climate = response.body.current.weather_descriptions;
+  console.log(' the temperature is '+ temperature +' and probability of rain is ' + probabilityRain);
+  console.log('today are ' + climate )
 });
